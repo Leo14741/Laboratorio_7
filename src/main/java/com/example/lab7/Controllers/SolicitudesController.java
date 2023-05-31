89d652923b2250190f3edfee64648660d54097e2
Leo14741 <a20201696@pucp.edu.pe>
2023-05-31 18:29:19 -0500

    Mitad de la 2

--- src/main/java/com/example/lab7/Controllers/SolicitudesController.java
+++ src/main/java/com/example/lab7/Controllers/SolicitudesController.java
@@ -1,9 +1,7 @@
 package com.example.lab7.Controllers;
 
 import com.example.lab7.Entitys.Solicitude;
-import com.example.lab7.Entitys.Usuario;
 import com.example.lab7.Repositorys.SolicitudeRepository;
-import com.example.lab7.Repositorys.UsuarioRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,45 +14,48 @@ import java.util.List;
 
 @RestController
 @Controller
-@RequestMapping("users")
-public class UserController {
-    final UsuarioRepository usuarioRepository;
+@RequestMapping("solicitudes")
+public class SolicitudesController {
+
     final SolicitudeRepository solicitudeRepository;
 
-    public UserController(UsuarioRepository usuarioRepository, SolicitudeRepository solicitudeRepository) {
-        this.usuarioRepository = usuarioRepository;
+    public SolicitudesController(SolicitudeRepository solicitudeRepository) {
         this.solicitudeRepository = solicitudeRepository;
     }
 
-    @GetMapping("/listar")
-    public List<Usuario> listarUsuarios(){
-        return usuarioRepository.findAll();
-    }
+    @GetMapping("/registro")
+    public List<Solicitude> listarSolicitudes() {return solicitudeRepository.findAll();}
 
-    @PostMapping("/crear")
-    public ResponseEntity<HashMap<String, Object>> guardarUsuario(
-            @RequestBody Usuario usuario,
-            @RequestParam(value = "fetchId", required = false) boolean fetchId) {
+    @PostMapping("/aprobarSolicitud")
+    public ResponseEntity<HashMap<String, Object>> guardarSolicitudes(
+            @RequestBody Solicitude solicitud) {
 
         HashMap<String, Object> responseJson = new HashMap<>();
 
-        usuarioRepository.save(usuario);
-        if(fetchId){
-            responseJson.put("id",usuario.getId());
+        if(solicitud.getSolicitudEstado().equals("")){
+            solicitud.setSolicitudEstado("pendiente");
         }
-        responseJson.put("estado","creado");
+        if(solicitud.getSolicitudEstado().equals("pendiente")){
+            solicitudeRepository.save(solicitud);
+            responseJson.put("Monto solicitado", solicitud.getSolicitudMonto());
+            responseJson.put("id",solicitud.getId());
+        }
+        else
+            responseJson.put("Solicitud ya atendida", solicitud.getId());
+
         return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
     }
+
+
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<HashMap<String,String>> gestionException(HttpServletRequest request) {
         HashMap<String, String> responseMap = new HashMap<>();
         if (request.getMethod().equals("POST")) {
             responseMap.put("estado", "error");
-            responseMap.put("msg", "Debe enviar un usuario");
+            responseMap.put("msg", "Debe enviar un solicitud");
         }
         return ResponseEntity.badRequest().body(responseMap);
     }
-
 
 }
