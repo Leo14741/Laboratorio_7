57285457eafc442c2af321f9a3c88731407c28b3
Carlos <cpisco@pucp.edu.pe>
2023-05-31 19:48:54 -0500

    Pregunta 2 al 90%

--- src/main/java/com/example/lab7/Controllers/SolicitudesController.java
+++ src/main/java/com/example/lab7/Controllers/SolicitudesController.java
@@ -7,10 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Controller
@@ -23,19 +25,17 @@ public class SolicitudesController {
         this.solicitudeRepository = solicitudeRepository;
     }
 
-    @GetMapping("/registro")
+    @GetMapping("/listar")
     public List<Solicitude> listarSolicitudes() {return solicitudeRepository.findAll();}
 
-    @PostMapping("/aprobarSolicitud")
+    @PostMapping("/registro")
     public ResponseEntity<HashMap<String, Object>> guardarSolicitudes(
             @RequestBody Solicitude solicitud) {
 
         HashMap<String, Object> responseJson = new HashMap<>();
 
-        if(solicitud.getSolicitudEstado().equals("")){
+        if (solicitud.getSolicitudEstado().equals("")) {
             solicitud.setSolicitudEstado("pendiente");
-        }
-        if(solicitud.getSolicitudEstado().equals("pendiente")){
             solicitudeRepository.save(solicitud);
             responseJson.put("Monto solicitado", solicitud.getSolicitudMonto());
             responseJson.put("id",solicitud.getId());
@@ -46,6 +46,59 @@ public class SolicitudesController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
     }
 
+    @PutMapping("/aprobarSolicitud")
+    public ResponseEntity<HashMap<String, Object>> aprobarSolicitudes(Model model,
+                                                                      @RequestParam("idSolicitud") int id) {
+
+
+        HashMap<String, Object> responseJson = new HashMap<>();
+        Optional<Solicitude> optionalSolicitude = solicitudeRepository.findById(id);
+        if(optionalSolicitude.isPresent()) {
+            Solicitude solicitud = optionalSolicitude.get();
+            if (solicitud.getSolicitudEstado().equals("pendiente")) {
+                solicitud.setSolicitudEstado("aprobada");
+                responseJson.put("id", solicitud.getId());
+            }
+            else
+                responseJson.put("Solicitud ya atendida", solicitud.getId());
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+    }
+
+    @PutMapping("/denegarSolicitud")
+    public ResponseEntity<HashMap<String, Object>> denegarSolicitudes(Model model,
+                                                                      @RequestParam("idSolicitud") int id)  {
+
+        HashMap<String, Object> responseJson = new HashMap<>();
+        Optional<Solicitude> optionalSolicitude = solicitudeRepository.findById(id);
+        if(optionalSolicitude.isPresent()) {
+            Solicitude solicitud = optionalSolicitude.get();
+            if (solicitud.getSolicitudEstado().equals("pendiente")) {
+                solicitud.setSolicitudEstado("denegada");
+                responseJson.put("id", solicitud.getId());
+            }
+            else
+                responseJson.put("Solicitud ya atendida", solicitud.getId());
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+    }
+
+    @DeleteMapping("/eliminarSolicitud")
+    public ResponseEntity<HashMap<String, Object>> deleteSolicitudes(Model model,
+                                                                      @RequestParam("idSolicitud") int id)  {
+
+        HashMap<String, Object> responseJson = new HashMap<>();
+        Optional<Solicitude> optionalSolicitude = solicitudeRepository.findById(id);
+        if(optionalSolicitude.isPresent()) {
+            Solicitude solicitud = optionalSolicitude.get();
+            if (solicitud.getSolicitudEstado().equals("") || solicitud.getSolicitudEstado().equals("pendiente")) {
+                solicitudeRepository.deleteById(id);
+                responseJson.put("id", solicitud.getId());
+            } else
+                responseJson.put("Solicitud ya atendida", solicitud.getId());
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+    }
 
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
